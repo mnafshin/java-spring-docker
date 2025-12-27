4e01a9ae976167c42b5a346d7bb2e28e0c7e174d
Afshin.Amini <mnafshin@gmail.com>
2025-12-28 00:27:29 +0100

    init commit

---
+++ src/main/java/io/github/mnafshin/java_spring_docker/DemoJavaSpringDockerApplication.java
@@ -0,0 +1,13 @@
+package io.github.mnafshin.java_spring_docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJavaSpringDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJavaSpringDockerApplication.class, args);
+	}
+
+}
